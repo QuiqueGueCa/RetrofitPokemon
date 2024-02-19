e4e3cb25892858633144f326514962b2c92dc1f7
QuiqueGueCa <enrique.guevara@vasscompany.com>
2024-02-19 09:50:12 +0100

    abilities in linear instead in recyclerView

--- app/src/main/java/com/example/retrofitpokemon/ui/detail_fragment/DetailFragment.kt
+++ app/src/main/java/com/example/retrofitpokemon/ui/detail_fragment/DetailFragment.kt
@@ -10,10 +10,10 @@ import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.retrofitpokemon.R
+import com.example.retrofitpokemon.data.domain.model.ability_detail.AbilityDetailModel
 import com.example.retrofitpokemon.data.domain.model.evolution_chain_detail.EvolutionChainDetailModel
 import com.example.retrofitpokemon.data.domain.model.pokemon_detail.PokemonDetailModel
 import com.example.retrofitpokemon.data.domain.usecase.GetAbilityDetailUseCase
@@ -21,14 +21,13 @@ import com.example.retrofitpokemon.data.domain.usecase.GetEvolutionChainDetailUs
 import com.example.retrofitpokemon.data.domain.usecase.GetPokemonDetailUseCase
 import com.example.retrofitpokemon.data.domain.usecase.GetPokemonSpeciesUseCase
 import com.example.retrofitpokemon.databinding.FragmentDetailBinding
+import com.example.retrofitpokemon.databinding.ItemAbilityBinding
 import com.example.retrofitpokemon.databinding.ItemEvolutionBinding
 import com.example.retrofitpokemon.injection.InjectionSingleton
-import com.example.retrofitpokemon.ui.detail_fragment.adapter.AbilityAdapter
 import kotlinx.coroutines.launch
 
 class DetailFragment : Fragment() {
 
-    private lateinit var mAdapter: AbilityAdapter
     private val args: DetailFragmentArgs by navArgs()
     private val mViewModel: DetailFragmentViewModel =
         DetailFragmentViewModel(
@@ -52,22 +51,11 @@ class DetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        setupAdapter()
-
         setupViewModel()
 
         mViewModel.getPokemonDetail(args.position)
     }
 
-    private fun setupAdapter() {
-        mAdapter = AbilityAdapter(mutableListOf())
-
-        with(mBinding) {
-            recyclerView.setHasFixedSize(true)
-            recyclerView.layoutManager = LinearLayoutManager(requireContext())
-            recyclerView.adapter = mAdapter
-        }
-    }
 
     private fun setupViewModel() {
 
@@ -94,10 +82,25 @@ class DetailFragment : Fragment() {
                         mBinding.vBackground.visibility = View.GONE
 
                         setupPokemonMainData(uiState.pokemonDetailModel)
-                        mAdapter.refreshData(uiState.abilities)
+                        showAbilities(uiState.abilities)
                         showEvolutionChain(uiState.evolutionChainDetailModel)
                     }
                 }
+            }
+        }
+    }
+
+    private fun showAbilities(abilities: MutableList<AbilityDetailModel>) {
+        for (ability in abilities) {
+            val itemBinding = ItemAbilityBinding.inflate(layoutInflater)
+            mBinding.llAbilities.addView(itemBinding.root)
+            val param = itemBinding.root.layoutParams as ViewGroup.MarginLayoutParams
+            param.setMargins(0, 16, 0, 16)
+
+            with(itemBinding) {
+                root.layoutParams = param
+                tvNameAbility.text = ability.name
+                tvDescriptionAbility.text = ability.effectEntriesModel.effect
             }
         }
     }
